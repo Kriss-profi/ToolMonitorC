ae85589b9f0266c7fef0c3ced1f497cdd859cecd
Krzysztof Jakubowski <jakubowski@polprofi.de>
2022-01-31 20:39:33 +0100

    Created Resources (Employee, User, Tool)

---
+++ ToolMonitorC/Entities/Tool.cs
@@ -0,0 +1,11 @@
+﻿namespace ToolMonitorC.Entities
+{
+    internal class Tool : EntityBase
+    {
+        public string ToolName { get; set; }
+        public string ToolDescription { get; set; }
+        public int Varanty { get; set; }
+
+        public override string ToString() => $"Id: {Id} | FirstName: {ToolName} \t | Opis: {ToolDescription} \t | Gwarancja: {Varanty}";
+    }
+}
---
+++ ToolMonitorC/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ToolMonitorC.Entities
+{
+    internal class User : EntityBase
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} | Name: {UserName} \t | email: {Email}";
+        }
+    }
+}
