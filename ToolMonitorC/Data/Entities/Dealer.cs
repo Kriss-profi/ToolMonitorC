0d8a4c88c51a2266e6e73fe3018eb9739af9dca2
Krzysztof Jakubowski <jakubowski@polprofi.de>
2022-06-20 13:43:01 +0200

    Added new Dealer class

---
+++ ToolMonitorC/Data/Entities/Dealer.cs
@@ -0,0 +1,11 @@
+﻿using ToolMonitorC.Entities;
+
+namespace ToolMonitorC.Data.Entities
+{
+    public class Dealer : EntityBase
+    {
+        public string Name { get; set; }
+        public string Web { get; set; }
+
+    }
+}
