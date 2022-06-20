1d06f8d3277ab87754513779a36ebd3cdd099199
Krzysztof Jakubowski <jakubowski@polprofi.de>
2022-06-20 13:46:31 +0200

    Libraries updated

--- ToolMonitorC/Program.cs
+++ ToolMonitorC/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using ToolMonitorC.ConsolMenu;
 using ToolMonitorC.Data;
 using ToolMonitorC.Entities;
 using ToolMonitorC.Repositories;
