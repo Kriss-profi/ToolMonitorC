ddfe18854157369ee520ea18ed2b7bb95b179601
Krzysztof Jakubowski <jakubowski@polprofi.de>
2022-09-01 02:34:06 +0200

    DI Menu

--- ToolMonitorC/Program.cs
+++ ToolMonitorC/Program.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ToolMonitorC.ConsolMenu;
 using ToolMonitorC.Data;
 using ToolMonitorC.Data.Entities;
 using ToolMonitorC.Data.Repositories;
 using ToolMonitorC.UI;
+using ToolMonitorC.UI.EmployeeUi;
+using ToolMonitorC.UI.HelpersUi;
 
 var services = new ServiceCollection();
 services.AddSingleton<IApp, App>();
+services.AddSingleton<IMenu, Menu>();
+services.AddSingleton<IHelpMethod, HelpMethod>();
+services.AddSingleton<IEmployeeService, EmployeeService>();
 services.AddSingleton<IRepository<Employee>, ListRepository<Employee>>();
 services.AddSingleton<IRepository<Tool>, ListRepository<Tool>>();
 services.AddDbContext<ToolMonitorDbContext>(options => options.UseSqlServer("Data Source=KRISS\\SQLEXPRESS;Initial Catalog=ToolMonitorCStorage;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
 
 var serviceProvider = services.BuildServiceProvider();
 var app = serviceProvider.GetService<IApp>();
+//var EmployeeServices = serviceProvider.GetService<IEmployeeService>();
 
 app.Run();
